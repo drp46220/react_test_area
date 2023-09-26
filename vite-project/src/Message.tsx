9c2657b0d792ac8d92ba68b6e7f88ec534a98eb0
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-26 10:37:35 -0500

    little trinary helloworld statement

--- vite-project/src/Message.tsx
+++ vite-project/src/Message.tsx
@@ -3,7 +3,8 @@
 // PascalCasing
 function Message() {
     // JSX: javascript XML
-    return <h1> Hello World</h1>
+    const name = 'Dom';
+    return <h1> Hello {(name)?("world"):name}</h1>
 }
 
 // dont forget to export the function so its usable outside of this file
