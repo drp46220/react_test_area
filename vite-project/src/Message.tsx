28e8e9586a227f52200cd52669880bc4c89d45d0
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-26 10:23:17 -0500

    first hello world message

---
+++ vite-project/src/Message.tsx
@@ -0,0 +1,10 @@
+//function based component
+
+// PascalCasing
+function Message() {
+    // JSX: javascript XML
+    return <h1> Hello World</h1>
+}
+
+// dont forget to export the function so its usable outside of this file
+export default Message;
